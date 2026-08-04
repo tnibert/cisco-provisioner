f46b25cc6cbc623e80bcd8b6834aedf8ce86ee76
Timothy Nibert <docz2a@gmail.com>
2026-08-04 21:39:43 +1000

    add static routes to router

--- provisioning.py
+++ provisioning.py
@@ -11,9 +11,15 @@ hq_s000_ipv4 = IPAddress("172.16.30.1", 30)
 hq_g00_ipv4 = IPAddress("192.168.100.1", 24)
 hq_lo0_ipv4 = IPAddress("209.165.200.1", 24)
 branch_s000_ipv4 = IPAddress("172.16.30.2", 30)
+
 vlan_10_gw_ipv4 = IPAddress("192.168.10.1", 24)
 vlan_20_gw_ipv4 = IPAddress("192.168.20.1", 24)
 vlan_99_gw_ipv4 = IPAddress("192.168.99.1", 24)
+
+vlan_10_net_ipv4 = IPAddress("192.168.10.0", 24)
+vlan_20_net_ipv4 = IPAddress("192.168.20.0", 24)
+vlan_99_net_ipv4 = IPAddress("192.168.99.0", 24)
+
 switch_ipv4 = IPAddress("192.168.99.11", vlan_99_gw_ipv4.get_mask_len())
 default_route_net_ipv4 = IPAddress("0.0.0.0", 0)
 
@@ -47,7 +53,11 @@ devices = {
                      Port("g0/0", hq_g00_ipv4, None),
                      Port("s0/0/0", hq_s000_ipv4, None, dce=True)
                  ],
-                 [])
+                 [
+                     StaticIPv4Route(vlan_10_net_ipv4, branch_s000_ipv4),
+                     StaticIPv4Route(vlan_20_net_ipv4, branch_s000_ipv4),
+                     StaticIPv4Route(vlan_99_net_ipv4, branch_s000_ipv4),
+                 ])
 }
 
 if __name__=='__main__':
