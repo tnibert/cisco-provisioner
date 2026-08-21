e049eeeed5c8a117f28b71baf4d4022e39d97241
Timothy Nibert <docz2a@gmail.com>
2026-08-21 21:07:53 +1000

    dynamically load devices spec

--- provision.py
+++ provision.py
@@ -1,7 +1,7 @@
+#! /usr/bin/env python3
+
 import sys
-
-from examples.example2 import devices
-
+import importlib
 
 
 if __name__=='__main__':
@@ -9,8 +9,13 @@ if __name__=='__main__':
     generate provisioning commands
     """
     if len(sys.argv) > 1:
+        devices = importlib.import_module(sys.argv[1]).devices
+    else:
+        sys.exit("error: must specify description module\neg:\n./provision.py examples.example1")
+
+    if len(sys.argv) > 2:
         # provision specific device
-        print(devices[sys.argv[1]].provision())
+        print(devices[sys.argv[2]].provision())
     else:
         # all
         for k,v in devices.items():
