69489ee1de591a2136e00e1731a8504f8e379f26
Timothy Nibert <docz2a@gmail.com>
2026-09-02 17:52:16 +1000

    provide OSPF area via init parameters

--- ospf.py
+++ ospf.py
@@ -2,13 +2,44 @@ from functools import reduce
 from typing import List
 
 from ip import IPAddress
-from provisionable import Provisionable
+from provisionable import Provisionable, provision_group
 from templates.ospf import ospf_setup, network, passive_interface, v2_init, v3_init, ipv6_interface
 from templates.router import INTERFACE_BLOCK
 
 """
 todo: cost, bandwidth, and reference bandwidth modification is not yet implemented
 """
+
+class OSPFv2Network(Provisionable):
+    def __init__(self, net_addr: IPAddress, area=0):
+        self.net_addr = net_addr
+        self.area = area
+
+    def provision(self):
+        return network.format(
+            net_addr=self.net_addr.get_ip_addr(),
+            wildcard_mask=self.net_addr.get_wildcard_mask(),
+            area_id=self.area
+        )
+
+
+class OSPFv3Interface(Provisionable):
+    def __init__(self, intf_name, area=0, process_id=None):
+        self.intf = intf_name
+        self.area = area
+        self.process_id = process_id
+
+    def set_process_id(self, process_id) -> OSPFv3Interface:
+        self.process_id = process_id
+        return self
+
+    def provision(self):
+        return INTERFACE_BLOCK.format(
+            intf=self.intf,
+            body=ipv6_interface.format(process_id=self.process_id,
+                                       area_id=self.area)
+        )
+
 
 class OSPF(Provisionable):
     """
@@ -21,43 +52,31 @@ class OSPF(Provisionable):
 
 
 class OSPFv2(OSPF):
-    def __init__(self, networks: List[IPAddress], passive_interfaces: List[str], router_id, process_id):
+    def __init__(self, networks: List[OSPFv2Network], passive_interfaces: List[str], router_id, process_id):
         super().__init__(passive_interfaces, router_id, process_id)
         self.networks = networks if networks is not None else []
 
     def provision(self):
         return ospf_setup.format(version_init=v2_init.format(process_id=self.process_id),
                                  router_id=self.router_id,
-                                 networks=reduce(lambda a, x: a + x,
-                                                 map(lambda n: network.format(
-                                                     net_addr=n.get_ip_addr(),
-                                                     wildcard_mask=n.get_wildcard_mask(),
-                                                     area_id=0
-                                                 ),
-                                                     self.networks), ""),
+                                 networks=provision_group(self.networks),
                                  passive_interfaces=reduce(lambda a, x: a + x,
                                                            map(lambda p: passive_interface.format(intf=p),
                                                                self.passive_interfaces), ""),
                                  )
 
-class OSPFv3(OSPF):
-    def __init__(self, interfaces: List[str], passive_interfaces: List[str], router_id, process_id):
-        super().__init__(passive_interfaces, router_id, process_id)
-        self.interfaces = interfaces
 
-    def provision_interfaces(self) -> str:
-        return reduce(lambda a,x: a+x,
-                      map(lambda i: INTERFACE_BLOCK.format(intf=i,
-                                                           body=ipv6_interface.format(process_id=self.process_id,
-                                                                                      area_id=0)),
-                          self.interfaces),
-                      "")
+class OSPFv3(OSPF):
+    def __init__(self, interfaces: List[OSPFv3Interface], passive_interfaces: List[str], router_id, process_id):
+        super().__init__(passive_interfaces, router_id, process_id)
+        self.interfaces = list(map(lambda i: i.set_process_id(process_id), interfaces))
 
     def provision(self):
-        return ospf_setup.format(version_init=v3_init.format(process_id=self.process_id),
+        return ospf_setup.format(
+                          version_init=v3_init.format(process_id=self.process_id),
                           router_id=self.router_id,
                           networks="",
                           passive_interfaces=reduce(lambda a, x: a + x,
                                                     map(lambda p: passive_interface.format(intf=p),
                                                         self.passive_interfaces), ""),
-                          ) + self.provision_interfaces()
+                          ) + provision_group(self.interfaces)
